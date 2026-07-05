a37eb43f64b57b31144cd410c62fda1e5bb75e68
kristianbraila <tt8747430@gmail.com>
2026-07-05 15:19:06 +0200

    Revert "refactor: improve and add new features"
    This reverts commit 7fb93ecd62e129298895a38745087dd2eefb59a3.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app/App'
-import { initNativeShell } from './app/native'
 import './styles/index.css'
 
 const rootEl = document.getElementById('root')
 if (!rootEl) throw new Error('Root element #root not found')
-
-// Native-shell setup (hides the iOS keyboard accessory bar); a no-op on web.
-void initNativeShell()
 
 createRoot(rootEl).render(
   <StrictMode>
